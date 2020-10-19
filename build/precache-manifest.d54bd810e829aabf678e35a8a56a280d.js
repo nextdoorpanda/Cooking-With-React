d013f32e8c2d1000afddea81edbd38946dbda16e
Konstantinos Megas <nextdoorpanda@gmail.com>
2020-10-19 18:55:29 +0300

    Added build

---
+++ build/precache-manifest.d54bd810e829aabf678e35a8a56a280d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2b3ea3ed5dd46f7f58d23f9d41006c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90f7a147348750f9b3a4",
+    "url": "/static/js/2.2cb6b095.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2cb6b095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac68cc5c64da66382b64",
+    "url": "/static/js/main.08d2cb00.chunk.js"
+  },
+  {
+    "revision": "5690207e89eede46d165",
+    "url": "/static/js/runtime-main.197f46e8.js"
+  }
+]);
